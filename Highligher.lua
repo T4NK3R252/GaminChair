4c066754c4fc7445eeda30c9f9b8a3bd5bc5a84a
T4NK3R252 <71943380+T4NK3R252@users.noreply.github.com>
2023-09-30 18:09:41 -0400

    Update Highligher.lua

--- Highligher.lua
+++ Highligher.lua
@@ -1,46 +1,126 @@
--- Function to create a highlight part for a player
-local function CreateHighlightPart(player)
-    local character = player.Character
-    if not character then
-        return
+local dwEntities = game:GetService("Players")
+local dwLocalPlayer = dwEntities.LocalPlayer 
+local dwRunService = game:GetService("RunService")
+
+local settings_tbl = {
+    ESP_Enabled = true,
+    ESP_TeamCheck = false,
+    Chams = true,
+    Chams_Color = Color3.fromRGB(0,0,255),
+    Chams_Transparency = 0.1,
+    Chams_Glow_Color = Color3.fromRGB(255,0,0)
+}
+
+function destroy_chams(char)
+
+    for k,v in next, char:GetChildren() do 
+
+        if v:IsA("BasePart") and v.Transparency ~= 1 then
+
+            if v:FindFirstChild("Glow") and 
+            v:FindFirstChild("Chams") then
+
+                v.Glow:Destroy()
+                v.Chams:Destroy() 
+
+            end 
+
+        end 
+
+    end 
+
+end
+
+dwRunService.Heartbeat:Connect(function()
+
+    if settings_tbl.ESP_Enabled then
+
+        for k,v in next, dwEntities:GetPlayers() do 
+
+            if v ~= dwLocalPlayer then
+
+                if v.Character and
+                v.Character:FindFirstChild("HumanoidRootPart") and 
+                v.Character:FindFirstChild("Humanoid") and 
+                v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+
+                    if settings_tbl.ESP_TeamCheck == false then
+
+                        local char = v.Character 
+
+                        for k,b in next, char:GetChildren() do 
+
+                            if b:IsA("BasePart") and 
+                            b.Transparency ~= 1 then
+                                
+                                if settings_tbl.Chams then
+
+                                    if not b:FindFirstChild("Glow") and
+                                    not b:FindFirstChild("Chams") then
+
+                                        local chams_box = Instance.new("BoxHandleAdornment", b)
+                                        chams_box.Name = "Chams"
+                                        chams_box.AlwaysOnTop = true 
+                                        chams_box.ZIndex = 4 
+                                        chams_box.Adornee = b 
+                                        chams_box.Color3 = settings_tbl.Chams_Color
+                                        chams_box.Transparency = settings_tbl.Chams_Transparency
+                                        chams_box.Size = b.Size + Vector3.new(0.02, 0.02, 0.02)
+
+                                        local glow_box = Instance.new("BoxHandleAdornment", b)
+                                        glow_box.Name = "Glow"
+                                        glow_box.AlwaysOnTop = false 
+                                        glow_box.ZIndex = 3 
+                                        glow_box.Adornee = b 
+                                        glow_box.Color3 = settings_tbl.Chams_Glow_Color
+                                        glow_box.Size = chams_box.Size + Vector3.new(0.13, 0.13, 0.13)
+
+                                    end
+
+                                else
+
+                                    destroy_chams(char)
+
+                                end
+                            
+                            end
+
+                        end
+
+                    else
+
+                        if v.Team == dwLocalPlayer.Team then
+                            destroy_chams(v.Character)
+                        end
+
+                    end
+
+                else
+
+                    destroy_chams(v.Character)
+
+                end
+
+            end
+
+        end
+
+    else 
+
+        for k,v in next, dwEntities:GetPlayers() do 
+
+            if v ~= dwLocalPlayer and 
+            v.Character and 
+            v.Character:FindFirstChild("HumanoidRootPart") and 
+            v.Character:FindFirstChild("Humanoid") and 
+            v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                
+                destroy_chams(v.Character)
+
+            end
+
+        end
+
     end
-    
-    -- Create a new part to represent the highlight
-    local highlight = Instance.new("Part")
-    highlight.Anchored = true
-    highlight.CanCollide = false
-    highlight.Size = character.HumanoidRootPart.Size * 1.2 -- Adjust the size as needed
-    highlight.BrickColor = BrickColor.new("Bright red") -- Change the color as desired
-    highlight.Transparency = 0.5 -- Adjust the transparency as desired
-    highlight.Parent = workspace
 
-    -- Create a SurfaceGui to render the highlight through walls
-    local surfaceGui = Instance.new("SurfaceGui")
-    surfaceGui.Face = Enum.NormalId.Back -- Render through the back of walls
-    surfaceGui.Parent = highlight
-
-    local frame = Instance.new("Frame")
-    frame.Size = UDim2.new(1, 0, 1, 0)
-    frame.BackgroundTransparency = 1
-    frame.Parent = surfaceGui
-
-    -- Attach the highlight to the player's character
-    highlight.CFrame = character.HumanoidRootPart.CFrame
-
-    -- Remove the highlight after a certain duration (e.g., 5 seconds)
-    wait(0.1)
-    highlight:Destroy()
-end
-
--- Function to create a highlight part for all players
-local function CreateHighlightPartsForAllPlayers()
-    for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-        CreateHighlightPart(player)
-    end
-end
-
--- Run the CreateHighlightPartsForAllPlayers function periodically (e.g., every 10 seconds)
-while true do
-    CreateHighlightPartsForAllPlayers()
-    wait(0.1) -- Adjust the interval as desired
-end
+end)
