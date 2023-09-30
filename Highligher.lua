e8d31ec2b74a0b06deea2bc0d542cbc69dd53bbf
T4NK3R252 <71943380+T4NK3R252@users.noreply.github.com>
2023-09-30 17:26:46 -0400

    Update Highligher.lua

--- Highligher.lua
+++ Highligher.lua
@@ -1,31 +1,46 @@
--- Function to create a highlight UI for a player
-local function CreateHighlightUI(player)
-    local playerGui = player:WaitForChild("PlayerGui")
+-- Function to create a highlight part for a player
+local function CreateHighlightPart(player)
+    local character = player.Character
+    if not character then
+        return
+    end
     
-    -- Create a BillboardGui
-    local billboardGui = Instance.new("BillboardGui")
-    billboardGui.Size = UDim2.new(1, 0, 1, 0)
-    billboardGui.Adornee = player.Character.Head
-    billboardGui.Parent = playerGui
+    -- Create a new part to represent the highlight
+    local highlight = Instance.new("Part")
+    highlight.Anchored = true
+    highlight.CanCollide = false
+    highlight.Size = character.HumanoidRootPart.Size * 1.2 -- Adjust the size as needed
+    highlight.BrickColor = BrickColor.new("Bright red") -- Change the color as desired
+    highlight.Transparency = 0.5 -- Adjust the transparency as desired
+    highlight.Parent = workspace
 
-    -- Create a Frame to represent the highlight
-    local highlightFrame = Instance.new("Frame")
-    highlightFrame.Size = UDim2.new(1.5, 0, 1.5, 0) -- Adjust the size as needed
-    highlightFrame.Position = UDim2.new(-0.25, 0, -0.25, 0)
-    highlightFrame.BackgroundColor3 = Color3.new(1, 0, 0) -- Change the color as desired
-    highlightFrame.BackgroundTransparency = 0.5 -- Adjust the transparency as desired
-    highlightFrame.Parent = billboardGui
+    -- Create a SurfaceGui to render the highlight through walls
+    local surfaceGui = Instance.new("SurfaceGui")
+    surfaceGui.Face = Enum.NormalId.Back -- Render through the back of walls
+    surfaceGui.Parent = highlight
+
+    local frame = Instance.new("Frame")
+    frame.Size = UDim2.new(5, 0, 10, 0)
+    frame.BackgroundTransparency = 1
+    frame.Parent = surfaceGui
+
+    -- Attach the highlight to the player's character
+    highlight.CFrame = character.HumanoidRootPart.CFrame
+
+    -- Remove the highlight after a certain duration (e.g., 5 seconds)
+    wait(5)
+    highlight:Destroy()
 end
 
--- Function to create highlight UI for all players
-local function CreateHighlightUIForAllPlayers()
+-- Function to create a highlight part for all players
+local function CreateHighlightPartsForAllPlayers()
     for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-        CreateHighlightUI(player)
+        CreateHighlightPart(player)
     end
 end
 
--- Run the CreateHighlightUIForAllPlayers function when a player joins the game
-game:GetService("Players").PlayerAdded:Connect(CreateHighlightUI)
-
--- You can also run it for existing players when the script starts
-CreateHighlightUIForAllPlayers()
+-- Run the CreateHighlightPartsForAllPlayers function periodically (e.g., every 10 seconds)
+while true do
+    CreateHighlightPartsForAllPlayers()
+    wait(10) -- Adjust the interval as desired
+end
