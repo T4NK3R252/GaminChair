5ae1ed4f58b869b9fe0ed9ecb053bff743150f87
T4NK3R252 <71943380+T4NK3R252@users.noreply.github.com>
2023-09-30 20:13:49 -0400

    Update AimAssist.lua

--- AimAssist.lua
+++ AimAssist.lua
@@ -1,1 +1,28 @@
--- aim assist script here
+local camera = game.CurrentCamera
+local UIS = game.UserInputService
+
+function getClosest()
+  local closestDistance = math.huge
+  local closestPlayer = nil
+  for i,v in pairs(game.Players:GetChildren()) do
+    if v ~= game.Players.LocalPlayer and v.Team ~= game.Players.LocalPlayers.Team then
+      if distance < closestDistance then
+        closestDistance = distance
+        closestPlayer = v
+      end
+    end
+  end
+end
+
+UIS.InputBegan:Connect(function(input)
+  if input.UserInputType == Enum.UserInputType.LeftShift then
+      _G.aim = true
+      while wait() do
+        camera.CFrame = CFrame.new(camera.Position, getClosest().Head.Position)
+        if _G.aim == false then return end
+  end
+end)
+UIS.InputEnded:Connect(function(input)
+  if input.UserInputType == Enum.UserInputType.LeftShift then
+        _G.aim = false
+end)
